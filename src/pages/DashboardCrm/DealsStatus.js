2557f0ee951fe5801c982bd836cb272c4b241938
Sroya <shazibbilal25@gmail.com>
2023-12-12 16:37:49 +0500

    ui cheange button

--- src/pages/DashboardCrm/DealsStatus.js
+++ src/pages/DashboardCrm/DealsStatus.js
@@ -62,7 +62,7 @@ const DealsStatus = () => {
                         <div className="flex-shrink-0">
                             <button type="button" onClick={handleClick} className="btn btn-soft-secondary btn-sm">
 
-                            Add New  
+                            Add New  Button
                              {/* adding new comment */}
                                 {/* <i className="ri-file-list-3-line align-middle"></i> Add New */}
                             </button>
