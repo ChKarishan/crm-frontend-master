e25d67dd6bedb33c1976616a7796acca16b623a5
Sroya <shazibbilal25@gmail.com>
2023-12-22 18:43:52 +0500

    button in deals details UI fix

--- src/pages/Ecommerce/EcommerceOrders/EcommerceOrderDetail.js
+++ src/pages/Ecommerce/EcommerceOrders/EcommerceOrderDetail.js
@@ -32,7 +32,7 @@ const EcommerceOrderDetail = (props) => {
   const handleClick = () => {
     console.log("Button clinck");
     // Use the history object to navigate to the other page
-    let path = `/apps-ecommerce-products`; 
+    let path = `/apps-crm-deals`; 
     navigate(path);
   };
 
@@ -57,7 +57,7 @@ const EcommerceOrderDetail = (props) => {
                         <div className="flex-shrink-0">
                             <button type="button" onClick={handleClick} className="btn btn-soft-secondary btn-sm">
 
-                            Add Installers
+                            Add New Deal
                                 
                             </button>
                         </div>
